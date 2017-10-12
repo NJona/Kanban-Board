ff87e72e4434ae0053d5c388be4b6af32a3494bc
Jona <Jona.Neef.97@gmail.com>
2017-10-12 18:42:24 +0200

    create get/set for Column, create Unit Test for Column

--- Implementation/Kanban-Board/src/Column.java
+++ Implementation/Kanban-Board/src/Column.java
@@ -2,6 +2,13 @@
 public class Column {
 	String name;
 	public Column(String name){
-		
+		this.name = name;
+	}
+	
+	String getName(){
+		return this.name;
+	}
+	void setName(String name){
+		this.name = name;
 	}
 }
